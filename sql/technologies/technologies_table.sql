b799ecf91e9fb5f4e3c3b2961a5ca1d9a237903d
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-02-11 13:27:09 -0300

    feat: create tables and diagrams

---
+++ sql/technologies/technologies_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE technologies (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+INSERT INTO technologies (name)
+VALUES
+  ('JavaScript'),
+  ('Python'),
+  ('React'),
+  ('Express.js'),
+  ('HTML'),
+  ('CSS'),
+  ('Django'),
+  ('PostgreSQL'),
+  ('MongoDB');
